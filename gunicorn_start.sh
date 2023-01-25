1805b5b3e630940f80c9a11abda8d215f6d7ecf0
Nicolas Aspert <nicolas.aspert@epfl.ch>
2023-01-25 13:47:56 +0100

    increase timeout

--- gunicorn_start.sh
+++ gunicorn_start.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 export FLASK_CONFIG=PRODUCTION
-SCRIPT_NAME=/theia /opt/conda/envs/theia/bin/gunicorn 'theia.web:create_app()' -w 10 -b 0.0.0.0:8000 --log-file /www/theia/logs/dbg.log --log-level debug
+SCRIPT_NAME=/theia /opt/conda/envs/theia/bin/gunicorn 'theia.web:create_app()' -w 10 -b 0.0.0.0:8000 --timeout 180 --log-file /www/theia/logs/dbg.log --log-level debug
