73781e4cc94b1e7cdd42e388da37f21fe66a72f3
Nicolas Aspert <nicolas.aspert@epfl.ch>
2023-01-25 13:19:36 +0100

    increase log level + num workers

--- gunicorn_start.sh
+++ gunicorn_start.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 export FLASK_CONFIG=PRODUCTION
-SCRIPT_NAME=/theia /opt/conda/envs/theia/bin/gunicorn 'theia.web:create_app()' -w 3 -b 0.0.0.0:8000
+SCRIPT_NAME=/theia /opt/conda/envs/theia/bin/gunicorn 'theia.web:create_app()' -w 10 -b 0.0.0.0:8000 --log-file /www/theia/logs/dbg.log --log-level debug
